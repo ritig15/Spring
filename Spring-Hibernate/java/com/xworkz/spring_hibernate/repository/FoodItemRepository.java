2cda5848f3e34035e6ebc9677c45715d8914f9e9
ritig15 <79107814+ritig15@users.noreply.github.com>
2021-04-09 17:43:23 +0530

    Delete Spring-Hibernate/java/com/xworkz/spring_hibernate/repository directory

--- Spring-Hibernate/java/com/xworkz/spring_hibernate/repository/FoodItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.spring_hibernate.repository;
-
-import com.xworkz.spring_hibernate.entity.FoodItemEntity;
-
-public interface FoodItemRepository {
-
-	public void save(FoodItemEntity entity);
-}
